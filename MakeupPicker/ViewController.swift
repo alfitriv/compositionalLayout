aa694f11d2a89553f541e3f7a8b447f06664ab3a
Mila Alfitri <alfitriv@gmail.com>
2020-03-19 21:14:27 +0700

    Initial Commit

---
+++ MakeupPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeupPicker
+//
+//  Created by Vania Radmila Alfitri on 19/03/20.
+//  Copyright © 2020 Vania Radmila Alfitri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
